71c77975f8de18a40b19d53bc2eab5fc102ace3f
Jeroen Vijgen <jjvijgen@gmail.com>
2020-02-28 00:05:16 +0100

    Added stable sorting to graph

--- src/Views/GraphView.tsx
+++ src/Views/GraphView.tsx
@@ -12,7 +12,26 @@ interface IData {
 export default class GraphView extends React.Component<{
 	data: any
 }> {
+    // Remove "year-" from the label, not required.
+    prepDate(date: string): string {
+        return date.substring(5, date.length);
+    }
+
     parseData(): Array<IData>{
+      // Sort data to coherently display in the Graph.
+      // eslint-disable-next-line
+      this.props.data.sort((a: any, b: any) => {
+        let dateA = new Date(a.date);
+        let dateB = new Date(b.date);
+        let same = dateA.getTime() === dateB.getTime();
+        // Date is eql, ignore sorting to stablize sorting.
+        if (same) return 0;
+        // Date A is greater than Date B.
+        if (dateA > dateB) return 1;
+        // Date A is lesser than Date B.
+        if (dateA < dateB) return -1;
+      });
+      
       // Object needing to be returned: 
       // { id: "missed_chat_count", data: [{x: int (DATE), y: int (Count)}] }
       // { id: "conversation_count", data: [{x: int (DATE), y: int (Count)}] }
@@ -22,9 +41,9 @@ export default class GraphView extends React.Component<{
       array.push({ id: "conversation_count", data: [] });
       array.push({ id: "visitors_with_conversation_count", data: [] });
       this.props.data.forEach((_item: any) => {
-        array[0].data.push({ x: _item.date, y: _item.missed_chat_count });
-        array[1].data.push({ x: _item.date, y: _item.conversation_count });
-        array[2].data.push({ x: _item.date, y: _item.visitors_with_conversation_count });
+        array[0].data.push({ x: this.prepDate(_item.date), y: _item.missed_chat_count });
+        array[1].data.push({ x: this.prepDate(_item.date), y: _item.conversation_count });
+        array[2].data.push({ x: this.prepDate(_item.date), y: _item.visitors_with_conversation_count });
       });
       return array;
     }
@@ -35,11 +54,11 @@ export default class GraphView extends React.Component<{
         <div className="GraphView">
             <ResponsiveAreaBump
                 data={this.parseData()}
-                margin={{ top: 40, right: 100, bottom: 40, left: 100 }}
+                margin={{ top: 40, right: 60, bottom: 40, left: 60 }}
                 spacing={4}
                 colors={{ scheme: 'nivo' }}
                 blendMode="multiply"
-                startLabel="id"
+                endLabel={false}
                 axisTop={{
                     tickSize: 5,
                     tickPadding: 5,
@@ -48,14 +67,7 @@ export default class GraphView extends React.Component<{
                     legendPosition: 'middle',
                     legendOffset: -36
                 }}
-                axisBottom={{
-                    tickSize: 5,
-                    tickPadding: 5,
-                    tickRotation: 0,
-                    legend: '',
-                    legendPosition: 'middle',
-                    legendOffset: 32
-                }}
+                axisBottom={null}
             />
         </div>
         )
